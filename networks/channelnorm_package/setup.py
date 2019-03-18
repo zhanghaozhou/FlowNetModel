5f1ca5f0810ba02d6144f0de9b6884eaac56cdb1
zhanghaozhou <zhanghaozhou428@gmail.com>
2019-03-18 15:26:14 +0100

    change gencode

--- networks/channelnorm_package/setup.py
+++ networks/channelnorm_package/setup.py
@@ -15,8 +15,7 @@ nvcc_args = [
     '-gencode', 'arch=compute_52,code=sm_52',
     '-gencode', 'arch=compute_60,code=sm_60',
     '-gencode', 'arch=compute_61,code=sm_61',
-    '-gencode', 'arch=compute_70,code=sm_70',
-    '-gencode', 'arch=compute_xx,code=sm_xx'
+    '-gencode', 'arch=compute_70,code=sm_70'
 ]
 
 setup(
